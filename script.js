b23404f888590872432015f57720b188c76e0232
vsergiors <vsergiobusiness@gmail.com>
2025-05-10 23:59:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript para interactividad opcional
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(event) {
+        alert("Gracias por visitar la página!");
+    });
+});
